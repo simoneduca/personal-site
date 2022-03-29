d09c1ccf46868d6be1a8810e247a51da5288840e
Simone Duca <simone.duca@engineering.digital.dwp.gov.uk>
2022-03-29 15:40:49 +0100

    chore: links styling. Adds link to /about.

--- _data/main.js
+++ _data/main.js
@@ -1,7 +1,7 @@
 module.exports = {
     en: {
         welcome: "Welcome",
-        siteDescription: `<p>I'm Simone, a web developer with an eye for accessibility and performance.</p>`,
+        siteDescription: `<p>I'm Simone, a web developer with an eye for accessibility and performance. <a class="c-underline-link" href="about">Read more.</a></p>`,
         latest: 'latest',
         post: 'post',
         posts: 'posts',
